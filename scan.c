623f73af3eefaba4fdd223969ba54b522a09c961
Sebastian Käwel <sebastian.kaewel@stud.thga.de>
2022-08-24 00:09:16 +0200

    optimierung Tabllendarstellung

--- scan.c
+++ scan.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <gtk/gtk.h>
+
 
 char const* const VERSIONSNR = "0.1.3-Aplpha";
 int laenge,laenge2,laenge3,laenge4,laenge5,laenge6,laenge7,laenge8,laenge9;
@@ -49,7 +49,7 @@ static void readcsv(char const* const datei)
         fprintf(stderr, "Couldnt open file '%s'\n", datei);
         exit(2);
     }
-    printf("Knotennr  |Vorname                                 |Nachname                                |Geburtsdatum                            |Todestag                      |Familienname                  |Beziehungsstatus                        |Adresse                            |Anz. Kinder\t|Kind von\t | \n"); 
+    printf("Knotennr  |Vorname                                 |Nachname                                |Geburtsdatum                            |Todestag                      |Familienname                            |Beziehungsstatus              |Adresse                                           |Anz. Kinder|Kind von| \n"); 
     printf("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
    person_t database[DB_SIZE];
     while(fscanf(filepointer,
@@ -67,16 +67,7 @@ static void readcsv(char const* const datei)
           != EOF)
 	   
     {
-	laenge=MyStrlen( database[zaehler].vorname );
-	laenge2=MyStrlen( database[zaehler].nachname );
-	laenge3=MyStrlen( database[zaehler].geburtsdatum );
-	laenge4=MyStrlen( database[zaehler].todestag );
-	laenge5=MyStrlen( database[zaehler].familienname );
-	laenge6=MyStrlen( database[zaehler].beziehungsstatus );
-	laenge7=MyStrlen( database[zaehler].adresse );
 	
-	
-		
         printf("|%d        |%*s|%*s|%*s|%*s|%*s|%*s|%*s|%d |%d | \n",
                database[zaehler].position,
 	       40,
@@ -87,11 +78,11 @@ static void readcsv(char const* const datei)
                database[zaehler].geburtsdatum,
 	       30,
 	       database[zaehler].todestag,
-	       30,
-               database[zaehler].familienname,
 	       40,
+               database[zaehler].familienname,
+	       30,
 	       database[zaehler].beziehungsstatus,
-	       20,
+	       50,
 	       database[zaehler].adresse,
 	       
 	       database[zaehler].kinder,
@@ -138,29 +129,6 @@ int main(int argc, char* argv[])
 
         case 'f':
             readcsv(optarg);
- /* GtkWidget *window;
-	    GtkWidget *fixed;
-	    GtkWidget *btn1;
-	    GtkWidget *btn2;
-	    GtkWidget *btn3;
-	    gtk_init(&argc, &argv);
-	    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	    gtk_window_set_title(GTK_WINDOW(window), "GtkFixed");
-	    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
-	    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-	    fixed = gtk_fixed_new();
-	    gtk_container_add(GTK_CONTAINER(window), fixed);
-	    btn1 = gtk_button_new_with_label("test");
-	    snprintf(buffer, sizeof(buffer), "%i",database[1].vorname);
-	    gtk_button_set_label(GTK_BUTTON(btn1),buffer);
-
-	    gtk_fixed_put(GTK_FIXED(fixed), btn1, 150, 50);
-	    gtk_widget_set_size_request(btn1, 80, 30);
-	    g_signal_connect(G_OBJECT(window), "destroy",G_CALLBACK(gtk_main_quit), NULL);
-	    gtk_widget_show_all(window);
-	    gtk_main();*/
-
-			
             break;
 
         case '?':
